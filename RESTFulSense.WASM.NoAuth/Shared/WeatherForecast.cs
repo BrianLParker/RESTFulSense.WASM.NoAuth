741c8b5953883607a5bef24887ee94b664ae26b3
Brian Parker <brian.parker@hotmail.com.au>
2021-12-12 04:37:01 +1100

    Add project files.

---
+++ RESTFulSense.WASM.NoAuth/Shared/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace RESTFulSense.WASM.NoAuth.Shared
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string? Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
